ed8c00e5dbc6606a67c647500676199df2550a4a
florian sudre <sudreflorianpro@gmail.com>
2022-04-20 11:12:00 +0200

    update view home

--- view/home.php
+++ view/home.php
@@ -6,16 +6,7 @@
                 <p>recherche alternance fullstack</p>
                 </a>
             </div>
-
-            <div>
-                <h1>Login Admin</h1>
-                <label><b>Nom Utilisater</b></label>
-                <input type="text" placeholder="Enter Votre userName" name="username" require>
-                <label><b>Mot de passe</b></label>
-                <input type="text" placeholder="Enter Votre mot de passe" name="password" require>
-                <input type="submit" id='submit' value='LOGIN' >
-
-            </div>
+        
         </section>
     </div>
 
