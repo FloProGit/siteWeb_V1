95e8f3ac7a6254375f799c41e67bbcf90eaf9bfc
florian sudre <sudreflorianpro@gmail.com>
2022-05-01 14:53:20 +0200

    test change uppercase folder name in git repository

---
+++ controller/login.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+include_once(CLASSES.'View.php');
+//include_once(MODEL.'loginModel.php');
+
+class Login 
+{
+
+    private const METHOD_GET = 'GET';
+    private const LOGIN_VIEW_NAME = 'login_view';
+    private $ModeLogin;
+
+    public function loginIn()
+    {
+        if($_SERVER['REQUEST_METHOD'] === self::METHOD_GET)
+        {
+            $data = [
+                'usernameError'=>'dqmlkdjqszpmdjqskmd',
+            ];
+            $myView = new View(self::LOGIN_VIEW_NAME);
+           
+            return $myView->render(['data' => $data]);
+        }
+        $username= $_POST['username'];
+        $password= $_POST['password'];
+        $this->ModeLogin = new loginModel($username,$password);
+        $datafetch = $this->ModeLogin->validateUser();
+
+        if($datafetch === 'NO USER FOUND')
+        {
+            echo 'NO USER FOUND';
+        }
+        else if($datafetch === true)
+        {
+            echo "VALIDATE";
+        }
+        else if($datafetch === false)
+        {
+            echo "wrong Password"; 
+        }
+      
+    }
+    
+     
+}
